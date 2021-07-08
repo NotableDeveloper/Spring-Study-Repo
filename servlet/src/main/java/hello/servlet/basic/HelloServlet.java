904e0bab9416fcee4ba95f31fba59e20b0618d2e
NotableDeveloper <castlerea@naver.com>
2021-07-08 11:00:03 +0900

    요청에 대한 start-line, header 정보 조회

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,16 +10,16 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
-        System.out.println("request = " + req);
-        System.out.println("response = " + resp);
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
         
-        String username = req.getParameter("username");
+        String username = request.getParameter("username");
         System.out.println("username = " + username);
 
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().write("hello " + username);
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
