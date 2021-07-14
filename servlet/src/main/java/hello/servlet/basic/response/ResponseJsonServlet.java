498d90a1cdfb601195b4e8e3df9ad31610821c0a
NotableDeveloper <castlerea@naver.com>
2021-07-14 19:52:41 +0900

    서블릿, JSP, MVC 패턴을 이용한 App 만들기

--- servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -10,13 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "responseJsonServlet", urlPatterns = "/response-json")
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
 
@@ -24,6 +25,7 @@ public class ResponseJsonServlet extends HttpServlet {
         helloData.setUsername("kim");
         helloData.setAge(20);
 
+        //{"username":"kim", "age":20}
         String result = objectMapper.writeValueAsString(helloData);
         response.getWriter().write(result);
     }
